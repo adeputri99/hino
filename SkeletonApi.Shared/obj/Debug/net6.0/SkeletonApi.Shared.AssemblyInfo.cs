89d0b2118c8b2352baf4cede8933883f62abd4c6
adeputri99 <ade02.eid@gmail.com>
2024-04-19 16:20:54 +0700

    new
    new

--- SkeletonApi.Shared/obj/Debug/net6.0/SkeletonApi.Shared.AssemblyInfo.cs
+++ SkeletonApi.Shared/obj/Debug/net6.0/SkeletonApi.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkeletonApi.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0dfcc55ef78428e28673728925397cfadfb4ab9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19c715da320c916411802729188cb548c978e2f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkeletonApi.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkeletonApi.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
