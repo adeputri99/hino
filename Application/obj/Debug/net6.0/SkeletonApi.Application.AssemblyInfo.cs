0002b411fbbcc622c1bef621ab5fd3477d743398
adeputri99 <ade02.eid@gmail.com>
2024-04-20 14:04:43 +0700

    new
    new2

--- Application/obj/Debug/net6.0/SkeletonApi.Application.AssemblyInfo.cs
+++ Application/obj/Debug/net6.0/SkeletonApi.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkeletonApi.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19c715da320c916411802729188cb548c978e2f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89d0b2118c8b2352baf4cede8933883f62abd4c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkeletonApi.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkeletonApi.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SkeletonApi.WebAPI/obj/Debug/net6.0/SkeletonApi.WebAPI.AssemblyInfo.cs
+++ SkeletonApi.WebAPI/obj/Debug/net6.0/SkeletonApi.WebAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkeletonApi.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19c715da320c916411802729188cb548c978e2f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89d0b2118c8b2352baf4cede8933883f62abd4c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkeletonApi.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkeletonApi.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
